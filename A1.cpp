026821e4a35c29852babea2b2095ce9955b7b48e
nguyentung158 <78079771+nguyentung158@users.noreply.github.com>
2021-03-08 11:00:01 +0700

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void inmang(int a[]){
+    cout << &a << endl;
+    cout << &a[0] << endl;
+
+
+}
+
+int main()
+{
+    int a[10];
+    inmang(a);
+
+}
